82397cb4600e9658dedf03c9c298eef32baff464
akamaluddin9 <ahmad.kmlddn@gmail.com>
2022-08-02 11:51:50 +0700

    revisi no3

--- no3/no3.js
+++ no3/no3.js
@@ -2,12 +2,14 @@ function isAnagram(word1,word2) {
 
   let kata1 = word1.trim().toLowerCase().replace(/\s/g,'');
   let kata2 = word2.trim().toLowerCase().replace(/\s/g,'');
+  
 
   for (let i = 0; i < kata2.length; i++){
-    if (kata1.indexOf(kata2[i]) === -1 )
+    if (kata1.length !== kata2.length || kata1.indexOf(kata2[i]) === -1)
       return false; 
   }
   return true;
+  console.log(kata1.length);
 }
 
 // Test Case
@@ -15,4 +17,4 @@ console.log(isAnagram('karet', 'raket')); // True
 console.log(isAnagram('Balok', 'Lobak')); // True
 console.log(isAnagram('Tom Marvolo Riddle', 'I am Lord Voldemort')); // True
 console.log(isAnagram('cicak', 'tengkorak')); // False
-console.log(isAnagram('cicak', 'tengkorak')); // False
+console.log(isAnagram('cicak', 'cicaaak')); // False
